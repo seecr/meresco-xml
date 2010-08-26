213da9baa9abd6c31eaabd0646fcb70a1559e971
thijsj <thijsj@83e7a582-4515-4195-8f39-222cbda6297e>
2010-08-26 13:39:49 +0000

    HM/TJ: added setup.py stuff.
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco-xml/trunk@2593 83e7a582-4515-4195-8f39-222cbda6297e

---
+++ setup.py
@@ -0,0 +1,39 @@
+## begin license ##
+#
+#    Meresco Xml is a set of components and tools for handling
+#    xml data objects.
+#    Copyright (C) 2010 Seek You Too (CQ2) http://www.cq2.nl
+#
+#    This file is part of Meresco Xml.
+#
+#    Meresco Xml is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    Meresco Xml is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Meresco Xml; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+from distutils.core import setup
+
+setup(
+    name='meresco-xml',
+    packages=[
+        'meresco.xml',
+    ],
+    version='%VERSION%',
+    url='http://www.meresco.org',
+    author='Seek You Too',
+    author_email='info@cq2.nl',
+    description='Meresco Xml is a set of components and tools for handling xml data objects.',
+    long_description='Meresco Xml is a set of components and tools for handling xml data objects.',
+    license='GNU Public License',
+    platforms='all',
+)
---
+++ testsetup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+## begin license ##
+#
+#    Meresco Xml is a set of components and tools for handling
+#    xml data objects.
+#    Copyright (C) 2010 Seek You Too (CQ2) http://www.cq2.nl
+#
+#    This file is part of Meresco Xml.
+#
+#    Meresco Xml is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    Meresco Xml is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Meresco Xml; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+## end license ##
+set -e
+
+rm -rf tmp build
+
+python setup.py install --root tmp
+
+cp meresco/__init__.py tmp/usr/lib/python2.5/site-packages/meresco
+export PYTHONPATH=`pwd`/tmp/usr/lib/python2.5/site-packages
+cp -r test tmp/test
+
+(
+cd tmp/test
+./alltests.sh
+)
+
+rm -rf tmp build
