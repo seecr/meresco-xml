a7d8899f30f1a28a7118d8a23108b262541436dc
Hendrik Moree <hendrik@seecr.nl>
2014-01-03 11:43:14 +0100

    HM: Python3 test

--- test/alltests.sh
+++ test/alltests.sh
@@ -26,12 +26,9 @@
 
 export LANG=en_US.UTF-8
 export PYTHONPATH=.:"$PYTHONPATH"
-pyversions="python2.6"
-if [ -e /usr/bin/python2.7 ]; then
-    pyversions="$pyversions python2.7"
-fi
+pyversions="python3.2"
 option=$1
-if [ "${option:0:10}" == "--python2." ]; then
+if [ "${option:0:10}" == "--python3." ]; then
     shift
     pyversions="${option:2}"
 fi
