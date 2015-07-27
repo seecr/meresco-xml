3d557479792d108443410552c3a12ac250c3851d
Seecr Development Team <development@seecr.nl>
2015-07-27 15:04:03 +0200

    HM/TJ: Normalize noMatchResult can be set to return another value.

--- meresco/xml/normalize.py
+++ meresco/xml/normalize.py
@@ -1,26 +1,28 @@
 ## begin license ##
 #
-#    Meresco Xml is a set of components and tools for handling
-#    xml data objects.
-#    Copyright (C) 2010 Seek You Too (CQ2) http://www.cq2.nl
+# "Meresco-Xml" is a set of components and tools for handling xml data objects.
 #
-#    This file is part of Meresco Xml.
+# Copyright (C) 2010 Seek You Too (CQ2) http://www.cq2.nl
+# Copyright (C) 2015 Seecr (Seek You Too B.V.) http://seecr.nl
 #
-#    Meresco Xml is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+# This file is part of "Meresco-Xml"
 #
-#    Meresco Xml is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# "Meresco-Xml" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with Meresco Xml; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# "Meresco-Xml" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Meresco-Xml"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
+
 from re import compile as _compile
 
 def compile(rule):
@@ -30,9 +32,12 @@ def compile(rule):
 		raise Exception(str(e) + ': ' + rule)
 
 class Normalize(object):
-	def __init__(self, rules):
+	_default = object()
+	def __init__(self, rules, noMatchResult=_default):
 		self.rules = [(compile(rule[0]),) + rule[1:] for rule in rules]
-	def process(self, value):
+		self._noMatchResult = noMatchResult
+
+	def process(self, value, noMatchResult=_default):
 		for rule in self.rules:
 			match = rule[0].match(value)
 			if match:
@@ -41,4 +46,10 @@ class Normalize(object):
 				else:
 					values = match.groups()
 				return rule[1] % values
-		return value
+		return self._noResult(noMatchResult, self._noMatchResult, value)
+
+	def _noResult(self, *args):
+		for a in args:
+			if a is not self._default:
+				return a
+		return args[-1]
--- test/normalizetest.py
+++ test/normalizetest.py
@@ -1,26 +1,28 @@
 ## begin license ##
 #
-#    Meresco Xml is a set of components and tools for handling
-#    xml data objects.
-#    Copyright (C) 2005-2010 Seek You Too (CQ2) http://www.cq2.nl
+# "Meresco-Xml" is a set of components and tools for handling xml data objects.
 #
-#    This file is part of Meresco Xml.
+# Copyright (C) 2005-2010 Seek You Too (CQ2) http://www.cq2.nl
+# Copyright (C) 2015 Seecr (Seek You Too B.V.) http://seecr.nl
 #
-#    Meresco Xml is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
+# This file is part of "Meresco-Xml"
 #
-#    Meresco Xml is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# "Meresco-Xml" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with Meresco Xml; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# "Meresco-Xml" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with "Meresco-Xml"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
+
 from unittest import TestCase
 from meresco.xml.normalize import Normalize
 
@@ -69,4 +71,15 @@ class NormalizeTest(TestCase):
 		normalize = Normalize(rules)
 		r1 = normalize.process('bOOm vuur v   ii   s')
 		self.assertEquals('oo UU ii', r1)
+
+	def testValueIfNoMatch(self):
+		normalize = Normalize([('(aap)', '%s')])
+		self.assertEquals('noot', normalize.process('noot'))
+		self.assertEquals(None, normalize.process('noot', noMatchResult=None))
+
+	def testNoMatchResult(self):
+		normalize = Normalize([('(aap)', '%s')], noMatchResult=None)
+		self.assertEquals(None, normalize.process('noot'))
+		self.assertEquals('vuur', normalize.process('noot', noMatchResult='vuur'))
+
 
