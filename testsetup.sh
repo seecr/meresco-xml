43aea499d1eb198f2ec02568c1c7084d24673a11
Seecr Development Team <development@seecr.nl>
2016-05-27 11:32:19 +0200

    HM: New style testsetup.sh

--- testsetup.sh
+++ testsetup.sh
@@ -4,7 +4,7 @@
 # "Meresco-Xml" is a set of components and tools for handling xml data objects.
 #
 # Copyright (C) 2010 Seek You Too (CQ2) http://www.cq2.nl
-# Copyright (C) 2013 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2013, 2016 Seecr (Seek You Too B.V.) http://seecr.nl
 #
 # This file is part of "Meresco-Xml"
 #
@@ -24,30 +24,20 @@
 #
 ## end license ##
 
+source /usr/share/seecr-tools/functions.d/test
+
 set -o errexit
 rm -rf tmp build
 mydir=$(cd $(dirname $0); pwd)
-source /usr/share/seecr-test/functions
 
-pyversions="2.6"
-if distro_is_debian_wheezy; then
-    pyversions="2.6 2.7"
-fi
-
-VERSION="x.y.z"
-
-for pyversion in $pyversions; do
-    definePythonVars $pyversion
-    echo "###### $pyversion, $PYTHON"
-    ${PYTHON} setup.py install --root tmp
-done
-cp -r test tmp/test
+definePythonVars
+$PYTHON setup.py install --root tmp
 removeDoNotDistribute tmp
+cp -r test tmp/test
 find tmp -name '*.py' -exec sed -r -e "
     s/\\\$Version:[^\\\$]*\\\$/\\\$Version: ${VERSION}\\\$/;
     " -i '{}' \;
 
-cp -r test tmp/test
 runtests "$@"
 rm -rf tmp build
 
