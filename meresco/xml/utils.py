6d5a2845241ca3ed62c8218aa0bdcc68ed764395
Thijs Janssen <thijs@seecr.nl>
2017-11-15 11:08:25 +0100

    TJ/JJ: remove control characters from text not allowed by lxml.

--- meresco/xml/utils.py
+++ meresco/xml/utils.py
@@ -2,7 +2,7 @@
 #
 # "Meresco-Xml" is a set of components and tools for handling xml data objects.
 #
-# Copyright (C) 2011, 2014 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2011, 2014, 2017 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2014 Stichting Bibliotheek.nl (BNL) http://www.bibliotheek.nl
 #
 # This file is part of "Meresco-Xml"
@@ -56,9 +56,14 @@ def createSubElement(parent, name, **kwargs):
 def createElement(name, text=None, attrib=None, nsmap=None, namespaces=_namespaces):
     element = Element(namespaces.curieToTag(name), nsmap=nsmap)
     if text is not None:
-        element.text = unicode(text)
+        element.text = removeControlCharacters(text)
     if attrib:
         for k,v in attrib.items():
             attr = namespaces.curieToTag(k) if ':' in k else k
-            element.attrib[attr] = unicode(v)
+            element.attrib[attr] = removeControlCharacters(v)
     return element
+
+_controlchars_replacement = {i:None for i in range(ord(' ')) if i not in [ord('\r'), ord('\t'), ord('\n')]}
+def removeControlCharacters(aString):
+    return unicode(aString).translate(_controlchars_replacement)
+
--- test/utilstest.py
+++ test/utilstest.py
@@ -1,30 +1,31 @@
 ## begin license ##
-# 
-# "Meresco-Xml" is a set of components and tools for handling xml data objects. 
-# 
-# Copyright (C) 2011 Seecr (Seek You Too B.V.) http://seecr.nl
-# 
+#
+# "Meresco-Xml" is a set of components and tools for handling xml data objects.
+#
+# Copyright (C) 2011, 2017 Seecr (Seek You Too B.V.) http://seecr.nl
+#
 # This file is part of "Meresco-Xml"
-# 
+#
 # "Meresco-Xml" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco-Xml" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco-Xml"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from seecr.test import SeecrTestCase
 
-from meresco.xml.utils import sortRootTagAttrib
+from meresco.xml.utils import sortRootTagAttrib, createElement, createSubElement
+from meresco.xml import namespaces
 
 class UtilsTest(SeecrTestCase):
     def testSortRootTagAttrib(self):
@@ -34,3 +35,18 @@ class UtilsTest(SeecrTestCase):
         self.assertEquals("""<a:root xmlns:a="namespace A" xmlns:b="namespace B">
     <b:sub>tag</b:sub>
 </a:root>""", sortRootTagAttrib(input))
+
+    def testCreateElement(self):
+        dc = createElement('oai_dc:dc', nsmap=namespaces.select('oai_dc', 'dc'))
+        createSubElement(dc, 'dc:title', text='<title>')
+        self.assertXmlEquals('<oai_dc:dc xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/" xmlns:dc="http://purl.org/dc/elements/1.1/"><dc:title>&lt;title&gt;</dc:title></oai_dc:dc>', dc)
+
+        self.assertXmlEquals('<dc:iets xmlns:dc="http://purl.org/dc/elements/1.1/" key="&lt;&quot;value&quot;>">te"xt</dc:iets>', createElement('dc:iets', attrib={'key':'<"value">'}, text='te"xt', nsmap=namespaces.select('dc')))
+
+    def testFixUnwantedChars(self):
+        dc = createElement('dc:title', nsmap=namespaces.select('dc'), text='\x0b\x1e\x1f\x01Text')
+        self.assertXmlEquals('<dc:title xmlns:dc="http://purl.org/dc/elements/1.1/">Text</dc:title>', dc)
+        dc = createElement('dc:title', nsmap=namespaces.select('dc'), attrib={'key':'\x0b\x1e\x1f\x01Text'})
+        self.assertXmlEquals('<dc:title xmlns:dc="http://purl.org/dc/elements/1.1/" key="Text"/>', dc)
+
+        
